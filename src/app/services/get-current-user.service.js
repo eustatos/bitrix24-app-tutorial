6a36ecef74a64f9177b6102be1e6dfa20d414c28
Aleksandr Astashkin <astashkinav@gmail.com>
2019-07-07 19:19:37 +0400

    refactor(): use BX24 promise

--- src/app/services/get-current-user.service.js
+++ src/app/services/get-current-user.service.js
@@ -1,21 +1,8 @@
-import { BX24 } from "bx24";
-import axios from "axios";
-
 function getCurrentUser() {
-  const bx24 = new BX24();
-  const urlParams = new URLSearchParams(window.location.search);
-  const baseUrl = `
-    ${urlParams.get("PROTOCOL") === 0 ? "https" : "http"}://${urlParams.get("DOMAIN")}
-`;
-
-
   const currentUserPromise = new Promise((resolve, reject) => {
-    bx24.getAuth().then(auth => {
-      axios({
-        method: "get",
-        url: baseUrl + "/rest/user.current?auth=" + auth.ACCESS_TOKEN
-      }).then(response => {
-        resolve(response.data.result);
+    BX24.init(function() {
+      BX24.callMethod("user.current", {}, function(res) {
+        resolve(res.answer.result);
       });
     });
   });
